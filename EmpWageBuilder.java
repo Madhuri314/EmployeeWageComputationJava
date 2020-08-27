5893bb47e0c71e718dad28d4f30319792dccc7e2
Madhuri Jagdeo <madhuri.jagdeo21@gmail.com>
2020-08-28 03:17:32 +0530

    Calculated Part Time Employee Wages

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,22 +1,30 @@
 public class EmpWageBuilder{
-        public static final int isPresent=1;
+        //Constants
+	public static final int isPartTime=1;
+        public static final int isFullTime=2;
         public static final int EMP_RATE_PER_HR=20;
 
         public static void main(String args[]){
-                int empHours=0;
+                //Variables
+		int empHours=0;
                 int empWagesPerDay=0;
+		
+		//random
+                double randomCheck=Math.floor(Math.random()*10)%3;
 
-                double randomCheck=Math.floor(Math.random()*10)%2;
-
-                if(randomCheck==isPresent){
-                	       System.out.println("Employee is present");
+                if(randomCheck==isFullTime){
+                        System.out.println("Employee is fulltime present");
                         empHours=8;
                 }
-                else{
-                           System.out.println("Employee is absent");
-	         empHours=0;
+                else if(randomCheck==isPartTime){
+                        System.out.println("Employee is present parttime");
+                        empHours=4;
                 }
-
+                else{
+                        System.out.println("Employee is absent");
+                        empHours=0;
+                }
+		//Computation
                 empWagesPerDay=empHours*EMP_RATE_PER_HR;
                 System.out.println("Employee Wage for a day: "+empWagesPerDay);
         }
