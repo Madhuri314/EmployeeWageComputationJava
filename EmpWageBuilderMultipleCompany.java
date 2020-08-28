d5d673ef4e4ecbc80c655dd199466427234b1028
Madhuri Jagdeo <madhuri.jagdeo21@gmail.com>
2020-08-29 01:42:22 +0530

    Day22 UC-8 Computed Employee wages for multiple companies

---
+++ EmpWageBuilderMultipleCompany.java
@@ -0,0 +1,74 @@
+public class EmpWageBuilderMultipleCompany {
+        private String companyName;
+        private int empRate;
+        private int numberofWorkingDays;
+        private int maxHrsInMonth;
+
+        /**
+        *added 1 new instance variable above ie.companyName and initialized
+        */
+        public EmpWageBuilderMultipleCompany(final String companyName,final int empRate, final int numberofWorkingDays,final int maxHrsInMonth) {
+                this.companyName=companyName;
+                this.empRate=empRate;
+                this.numberofWorkingDays=numberofWorkingDays;
+                this.maxHrsInMonth=maxHrsInMonth;
+        }
+
+        public static void main(String args[]) {
+                final EmpWageBuilderMultipleCompany ewbObj1= new EmpWageBuilderMultipleCompany("TATA", 20, 20, 100);
+                final int tataWages=ewbObj1.computeEmpWage();
+                System.out.println("Total TATA Employee wage: "+tataWages);
+
+                final EmpWageBuilderMultipleCompany ewbObj2= new EmpWageBuilderMultipleCompany("BATA", 15, 25, 100);
+                final int bataWages=ewbObj2.computeEmpWage();
+                System.out.println("Total BATA Employee wage: "+bataWages);
+
+        }
+
+        /**
+        *changed methodname to computeEmpWage
+        *@return totalWage
+        */
+        public int computeEmpWage() {
+                int totalWage=0;
+                int totalEmpHrs=0;
+                int totalWorkingDays=0;
+
+                while(totalEmpHrs<maxHrsInMonth && totalWorkingDays<numberofWorkingDays) {
+                        totalWorkingDays++;
+
+                        final int empHrs=getEmpHrs();
+                        final int empWage=empHrs*empRate;
+                        totalEmpHrs=empHrs;
+                        totalWage+=empWage;
+                }
+                return totalWage;
+        }
+
+        /**
+        *get employee hours
+        *@return employee hrs
+        */
+        public int getEmpHrs() {
+                final int isFullTime=1;
+                final int isPartTime=2;
+                int empHrs=0;
+
+                //get random value
+                final double randomValue=Math.floor(Math.random()*10)%3;
+
+                switch((int)randomValue) {
+                        case isFullTime:
+                 empHrs = 8;
+                 break;
+         case isPartTime:
+                 empHrs = 4;
+                 break;
+         default:
+                 empHrs = 0;
+                 break;
+                }
+                return empHrs;
+        }
+
+}
